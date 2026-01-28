6ce237158e038b705aab4237bfdbc76988fb7665
emilekao <emilioikao@gmail.com>
2026-01-28 13:23:44 +0000

    modification

--- app1.py
+++ app1.py
@@ -16,7 +16,7 @@ st.set_page_config(page_title="Loan Predictor Pro", page_icon="🏦", layout="wi
 @st.cache_data
 def load_and_clean_data():
     # UTILISE LE CHEMIN COMPLET AVEC 'r' DEVANT
-    path = r"C:\Users\emile\Downloads\loan_data(in).csv" 
+    path = "loan_data.csv"
     try:
         data = pd.read_csv(path, encoding='latin-1')
         
